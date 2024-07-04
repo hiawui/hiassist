39159b60edc49d44bbaf8a6fae1fd334d7f22b94
hiawui <hiawui@gmail.com>
2024-07-04 20:59:51 +0800

    优化闹钟界面逻辑

--- app/src/main/java/me/hiawui/hiassist/calendar/alarm/AlarmActivity.kt
+++ app/src/main/java/me/hiawui/hiassist/calendar/alarm/AlarmActivity.kt
@@ -19,12 +19,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -130,7 +131,7 @@ fun timeFlow(): Flow<String> = flow {
 
 @Composable
 fun MainInfo(title: String, modifier: Modifier) {
-    val time by timeFlow().collectAsState(initial = "")
+    val time by remember { timeFlow() }.collectAsState(initial = "")
     Column(modifier = modifier) {
         Row(
             modifier = Modifier
