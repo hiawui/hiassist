2dff0b3c9cc87b55e74e1fb36e493d7e8b7fd980
hiawui <hiawui@gmail.com>
2025-02-08 19:20:36 +0800

    增加刷新当年节假日功能

--- app/src/main/java/me/hiawui/hiassist/HomeActivity.kt
+++ app/src/main/java/me/hiawui/hiassist/HomeActivity.kt
@@ -1,49 +1,34 @@
 package me.hiawui.hiassist
 
-import android.app.AlarmManager
-import android.app.AlarmManager.AlarmClockInfo
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.icu.util.Calendar
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -51,10 +36,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import me.hiawui.hiassist.calendar.HolidayDataStore
 import me.hiawui.hiassist.calendar.MyCalendar
-import me.hiawui.hiassist.calendar.alarm.ACTION_ALARM
-import me.hiawui.hiassist.calendar.alarm.AlarmReceiver
+import me.hiawui.hiassist.calendar.updateHolidays
 import me.hiawui.hiassist.ui.theme.HiassistTheme
+import java.time.LocalDate
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,9 +54,9 @@ class HomeActivity : ComponentActivity() {
     }
 }
 
-enum class Page(val title: String, val seq: Int, val display: Boolean = true) {
-    Calendar(title = "Calender", seq = 0),
-    Greeting(title = "Set Alarm", seq = 1, display = false),
+enum class Page(val titleId: Int, val seq: Int, val display: Boolean = true) {
+    Calendar(titleId = R.string.nav_calendar, seq = 0),
+    Settings(titleId = R.string.nav_settings, seq = 1),
 }
 
 @Composable
@@ -99,16 +85,18 @@ fun AppScaffold(modifier: Modifier) {
 
 @Composable
 fun NavigationHost(modifier: Modifier = Modifier, navController: NavHostController) {
+    val routeCalendar = stringResource(id = R.string.nav_calendar)
+    val routeSettings = stringResource(id = R.string.nav_settings)
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = Page.Calendar.title
+        startDestination = routeCalendar,
     ) {
-        composable(Page.Calendar.title) {
+        composable(routeCalendar) {
             MyCalendar()
         }
-        composable(Page.Greeting.title) {
-            AlarmButton()
+        composable(routeSettings) {
+            MySettings()
         }
     }
 }
@@ -126,13 +114,14 @@ fun DrawerContent(
             .padding(top = 50.dp)
     ) {
         for (page in Page.entries.filter { it.display }.sortedBy { it.seq }) {
+            val route = stringResource(id = page.titleId)
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(50.dp)
             ) {
                 Text(
-                    text = page.title,
+                    text = route,
                     modifier = Modifier
                         .fillMaxWidth()
                         .align(Alignment.CenterVertically)
@@ -141,7 +130,7 @@ fun DrawerContent(
                             coroutineScope.launch {
                                 scaffoldState.drawerState.close()
                             }
-                            navController.navigate(page.title)
+                            navController.navigate(route)
                         },
                     fontSize = 18.sp,
                     textAlign = TextAlign.Start,
@@ -153,71 +142,30 @@ fun DrawerContent(
 }
 
 @Composable
-fun AlarmButton() {
+fun MySettings() {
     val context = LocalContext.current
-    val lifecycle = LocalLifecycleOwner.current.lifecycle
-    var isResumed by remember { mutableStateOf(false) }
-    DisposableEffect(lifecycle) {
-        val observer = LifecycleEventObserver { _, event ->
-            isResumed = (event == Lifecycle.Event.ON_RESUME)
-        }
-        lifecycle.addObserver(observer)
-        onDispose {
-            lifecycle.removeObserver(observer)
-        }
-    }
-
-    val hasOverlayPermission = Settings.canDrawOverlays(context)
-    LaunchedEffect(isResumed, Unit) {
-        if (!hasOverlayPermission) {
-            context.startActivity(
-                Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:${context.packageName}")
-                )
-            )
-        }
-    }
-
-    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    val calendar = Calendar.getInstance().apply {
-        add(Calendar.SECOND, 15)
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 100.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            val pi = PendingIntent.getBroadcast(
-                context,
-                0,
-                Intent(ACTION_ALARM, null, context, AlarmReceiver::class.java),
-                PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-            )
-            try {
-                alarmManager.setAlarmClock(AlarmClockInfo(calendar.timeInMillis, pi), pi)
-                logI { "setAlarmClock at ${calendar.time}" }
-            } catch (e: SecurityException) {
-                logE(e) { "setAlarmClock error!" }
+    val scope = rememberCoroutineScope()
+    val infoHolidayFetched = stringResource(id = R.string.info_holiday_fetched)
+    Surface {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                val year = LocalDate.now().year
+                scope.launch {
+                    try {
+                        context.updateHolidays(year, HolidayDataStore.fetchHolidays(year))
+                    } catch (e: HolidayDataStore.HolidayFetchingException) {
+                        logI { "fetching holidays ongoing" }
+                    }
+                    Toast.makeText(context, infoHolidayFetched, Toast.LENGTH_SHORT).show()
+                }
+            }) {
+                Text(text = stringResource(id = R.string.settings_fetch_holiday))
             }
-        }, enabled = hasOverlayPermission) {
-            Text(text = "Try Alarm")
-        }
-        Spacer(modifier = Modifier.height(200.dp))
-        Button(onClick = {
-            val pi = PendingIntent.getBroadcast(
-                context,
-                0,
-                Intent(ACTION_ALARM, null, context, AlarmReceiver::class.java).apply {
-                    putExtra("alarmId", 1111L)
-                },
-                PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-            )
-            alarmManager.cancel(pi)
-        }) {
-            Text(text = "Cancel Alarm")
         }
     }
 }
